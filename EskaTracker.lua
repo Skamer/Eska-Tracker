e7001f51734dd3e3dc9e6d7bdc8840e9d210a21c
Skamer <Skamer@users.noreply.github.com>
2020-10-16 17:35:04 +0200

    Fix the errors when opening the map

--- EskaTracker.lua
+++ EskaTracker.lua
@@ -75,14 +75,25 @@ end
 
 __SystemEvent__()
 function BLIZZARD_TRACKER_VISIBLITY_CHANGED(isVisible)
-  if isVisible then
+  local wasInitialized = false
+  if not ObjectiveTrackerFrame.initialized then 
     ObjectiveTracker_Initialize(ObjectiveTrackerFrame)
+    wasInitilized = true
+  end
+
+  if isVisible and not wasInitialized then
     ObjectiveTrackerFrame:SetScript("OnEvent", ObjectiveTracker_OnEvent)
+    WorldMapFrame:RegisterCallback("SetFocusedQuestID", ObjectiveTracker_OnFocusedQuestChanged, ObjectiveTrackerFrame)
+    WorldMapFrame:RegisterCallback("ClearFocusedQuestID", ObjectiveTracker_OnFocusedQuestChanged, ObjectiveTrackerFrame)
+    
     ObjectiveTrackerFrame:Show()
     ObjectiveTracker_Update()
   else
     ObjectiveTrackerFrame:Hide()
+
     ObjectiveTrackerFrame:SetScript("OnEvent", nil)
+    WorldMapFrame:RegisterCallback("SetFocusedQuestID", nil, ObjectiveTrackerFrame)
+    WorldMapFrame:RegisterCallback("ClearFocusedQuestID", nil, ObjectiveTrackerFrame)
   end
 end
 
